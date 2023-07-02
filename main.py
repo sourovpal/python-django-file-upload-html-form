de07ad825a86a043dcd00e152380f02b38dcdacf
Sourov Pal <85163889+sourovpal@users.noreply.github.com>
2023-07-02 08:38:16 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from django.core.files.storage import FileSystemStorage
+import uuid
 
 folder='my_folder/'
+
 myfile = request.FILES['image']
+
 fs = FileSystemStorage(location=folder)
-filename = fs.save(myfile.name, myfile)
-file_url = fs.url(filename)
+
+ext = myfile.name.split('.').pop()
+new_filename =  str(uuid.uuid4())+'.'+ ext
+
+filename = fs.save(new_filename, myfile)
+
+file_url = filename
